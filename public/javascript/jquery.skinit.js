442ea0149556541f036907fa28f6ce3fd4faae54
tuomashc <tuomas.heiskanen@siili.fi>
2014-11-12 15:39:36 +0200

    Converted into a jquery plugin

---
+++ public/javascript/jquery.skinit.js
@@ -0,0 +1,75 @@
+(function($){
+
+	$.fn.skinit = function(data){
+		var rootElement = $(this);
+		render(rootElement, data);
+		registerEvents();
+		return this;
+	}
+
+	function render(root, conf){
+		var rootElement = root;
+
+		// container for images
+		var layoutContainer = $('<div class="layout">');
+		layoutContainer.css('background-image', 'url(images/'+ conf.url +')');
+
+		// container for option controls
+		var optionsContainer = $('<div class="controls">');
+
+		rootElement.append(layoutContainer);
+		rootElement.append(optionsContainer);
+
+		// introduce layers
+		for(var i=0; i<conf.layers.length; i++){ 
+
+			var layer = conf.layers[i];
+
+			var layerElement = $('<div class="layer">');
+			layerElement.attr('id', 'layer-' + layer.id);
+
+			layoutContainer.append(layerElement);
+
+			var optionContainer = $('<div>');
+			var label = $('<label>');
+			label.text(layer.name);
+
+ 			var select = $('<select>');
+			select.attr('id', layer.id);
+
+			layer.options = layer.options.sort(function(a,b){
+				return a.url < b.url ? -1 : 1;
+			});
+
+			for(var j=0; j<layer.options.length; j++){
+				var option = layer.options[j];
+				var opt = $('<option>');
+				opt.attr('value', option.url);
+				opt.text(option.name);
+				select.append(opt);
+			}
+
+			optionContainer.append(label);
+			optionContainer.append(select)
+			optionsContainer.append(optionContainer);
+		}
+	}
+
+	function registerEvents(){
+
+		$('.controls').change(function(ev){
+
+			var target = $(ev.target);
+			var layerId = target.attr('id');
+			var filename = target.val();
+
+			if(!filename || filename.length == 0){
+				$('#layer-' + layerId).css('background-image', '');
+			}else{
+				$('#layer-' + layerId).css('background-image', 'url(./images/' + filename + ')');
+			}
+
+		});
+	}
+
+}(jQuery));
--- public/javascript/skinit.js
+++ public/javascript/skinit.js
@@ -1,76 +1,7 @@
 $(document).ready(function(){
-	console.log('document ready');
 
-	$.getJSON('javascript/skin_config.json', function(conf){
-		var config = conf;
-		render(config);
-		registerEvents();
+	$.getJSON('javascript/skin_config.json', function(data){
+		$('#skinit').skinit(data);
 	});
-
-	function render(conf){
-		var rootElement = $('#skinit');
-
-		// container for images
-		var layoutContainer = $('<div class="layout">');
-		layoutContainer.css('background-image', 'url(images/'+ conf.url +')');
-
-		// container for option controls
-		var optionsContainer = $('<div class="controls">');
-
-		rootElement.append(layoutContainer);
-		rootElement.append(optionsContainer);
-
-		// introduce layers
-		for(var i=0; i<conf.layers.length; i++){ 
-
-			var layer = conf.layers[i];
-
-			var layerElement = $('<div class="layer">');
-			layerElement.attr('id', 'layer-' + layer.id);
-
-			layoutContainer.append(layerElement);
-
-			var optionContainer = $('<div>');
-			var label = $('<label>');
-			label.text(layer.name);
-
- 			var select = $('<select>');
-			select.attr('id', layer.id);
-
-			layer.options = layer.options.sort(function(a,b){
-				return a.url < b.url ? -1 : 1;
-			});
-
-			for(var j=0; j<layer.options.length; j++){
-				var option = layer.options[j];
-				var opt = $('<option>');
-				opt.attr('value', option.url);
-				opt.text(option.name);
-				select.append(opt);
-			}
-
-			optionContainer.append(label);
-			optionContainer.append(select)
-			optionsContainer.append(optionContainer);
-		}
-	}
-
-	function registerEvents(){
-
-		$('.controls').change(function(ev){
-
-			console.log('change event');
-			var target = $(ev.target);
-			var layerId = target.attr('id');
-			var filename = target.val();
-
-			if(!filename || filename.length == 0){
-				$('#layer-' + layerId).css('background-image', '');
-			}else{
-				$('#layer-' + layerId).css('background-image', 'url(./images/' + filename + ')');
-			}
-
-		});
-	}
 
 });
